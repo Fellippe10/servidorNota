4b3a29c7a8be50589786cc5cd9d6d561b61b6e18
Fellippe10 <felipepalopolied@gmail.com>
2026-06-14 23:33:26 -0300

    fix: use publicCert parameter for xml-crypto v6 instead of custom keyInfoProvider to fix E0714 signature error

--- xml_signer.js
+++ xml_signer.js
@@ -1,22 +1,5 @@
 const { SignedXml } = require('xml-crypto');
 
-function CustomKeyInfoProvider(certificate) {
-    this.getKeyInfo = function (key, prefix) {
-        prefix = prefix || '';
-        prefix = prefix ? prefix + ':' : prefix;
-        // Limpar o certificado para deixar só o Base64 sem os cabeçalhos
-        const certBody = certificate
-            .replace(/-----BEGIN CERTIFICATE-----/g, '')
-            .replace(/-----END CERTIFICATE-----/g, '')
-            .replace(/\r?\n|\r/g, '');
-
-        return `<${prefix}X509Data><${prefix}X509Certificate>${certBody}</${prefix}X509Certificate></${prefix}X509Data>`;
-    };
-    this.getKey = function (keyInfo) {
-        return null; // Apenas usado na validação
-    };
-}
-
 /**
  * Assina digitalmente um XML no padrão exigido pelo Governo (ICP-Brasil).
  * @param {string} xml - O XML original (ex: <DPS>...</DPS>)
@@ -28,6 +11,7 @@ function CustomKeyInfoProvider(certificate) {
 function assinarXML(xml, privateKey, certificate, referenceId) {
     const sig = new SignedXml({
         privateKey: privateKey,
+        publicCert: certificate,
         signatureAlgorithm: 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256',
         canonicalizationAlgorithm: 'http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
     });
@@ -40,8 +24,6 @@ function assinarXML(xml, privateKey, certificate, referenceId) {
         ],
         digestAlgorithm: 'http://www.w3.org/2001/04/xmlenc#sha256'
     });
-    
-    sig.keyInfoProvider = new CustomKeyInfoProvider(certificate);
     
     sig.computeSignature(xml);
     return sig.getSignedXml();
