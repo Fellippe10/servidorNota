8bf579b374ad704f71271f7c90fbff381ee1bb38
Fellippe10 <felipepalopolied@gmail.com>
2026-06-15 01:00:48 -0300

    fix: remove cTribMun and test 990101 to bypass municipal check

--- index.js
+++ index.js
@@ -161,8 +161,7 @@ app.post('/emitir-nota', async (req, res) => {
         <cLocPrestacao>3303302</cLocPrestacao>
       </locPrest>
       <cServ>
-        <cTribNac>010401</cTribNac>
-        <cTribMun>010401</cTribMun>
+        <cTribNac>990101</cTribNac>
         <xDescServ>${servico}</xDescServ>
       </cServ>
     </serv>
